28eef28632fc4981d7edb6af6b3d34a625e06e15
Benson Li <bensonbinbinli@gmail.com>
2018-05-04 17:23:43 -0700

    add LogLoss function which gives me the the loss of a single trajectory

--- 3_gradient_intro/functions.py
+++ 3_gradient_intro/functions.py
@@ -6,24 +6,24 @@ import numpy as np
 ################################################################
 def getTrajectories(net,env):
     batch_size=2
-    traj_list=[]
+    trajectory_list=[]
     actions_list=[]
     reward_list=[]
     for _ in range(batch_size):
-        traj,actions,total_reward = sampleTrajectory(net,env)
+        trajectory,actions,total_reward = sampleTrajectory(net,env)
         ipdb.set_trace()
-        traj_list.append(traj) 
+        trajectory_list.append(trajectory) 
         actions_list.append(actions_list)
         reward_list.append(total_reward)
-    return np.array(traj_list),np.array(actions_list),np.array(reward_list)
+    return np.array(trajectory_list),np.array(actions_list),np.array(reward_list)
 
 #########################
 def sampleTrajectory(net,env):
-    traj=[]
+    trajectory=[]
     actions=[]
 
     state= env.reset()
-    traj.append(state)
+    trajectory.append(state)
     total_reward = 0
     while True:
         action = getAction(net,state) 
@@ -33,10 +33,10 @@ def sampleTrajectory(net,env):
         total_reward += reward
 
         if done == True:
-            assert len(traj) == len(actions), "Unequal states and actions!"
-            return traj,actions,total_reward
+            assert len(trajectory) == len(actions), "Unequal states and actions!"
+            return np.array(trajectory),np.array(actions),total_reward
         else:
-            traj.append(state)
+            trajectory.append(state)
 
 def getAction(net,state):
     state = numpyFormat(state).float()
@@ -45,3 +45,13 @@ def getAction(net,state):
     return np.argmax(output)
 #########################
 ################################################################
+
+def LogLoss(probs, actions, reward):
+    for i,(prob,action) in enumerate(zip(probs,actions)):
+        if i ==0:
+            total_loss = torch.log(prob[action])
+        else:
+            total_loss += torch.log(prob[action])
+    # negative in front of reward since optimizer does gradient descent
+    return torch.mul(total_loss,-reward)
+
