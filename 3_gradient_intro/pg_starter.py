6e5dde99e34a65bc8c7590496b188a91a53a4530
Benson Li <bensonbinbinli@gmail.com>
2018-05-06 20:47:59 -0700

    removed nesterov

--- 3_gradient_intro/pg_starter.py
+++ 3_gradient_intro/pg_starter.py
@@ -76,9 +76,9 @@ def trainModel(probability,neurons):
     baseline = -500
     num_trajectory = 20
     optimizer1 = optim.Adam(net.parameters(), lr=0.01)
-    optimizer2 = optim.SGD(net.parameters(),  lr=1e-4,momentum=0.8,nesterov=True)
-    scheduler2 = LambdaLR(optimizer2,lr_lambda=cyclic(80))
-    optimizer3 = optim.RMSprop(net.parameters(), lr=0.01,alpha=0.95)
+    optimizer2 = optim.SGD(net.parameters(),  lr=1e-4,momentum=0.8)
+    scheduler2 = LambdaLR(optimizer2,lr_lambda=cyclic(100))
+    optimizer3 = optim.RMSprop(net.parameters(), lr=0.001,alpha=0.95)
     for episode in range(num_episodes):
         # print(episode)
         # before_weights_list = layerMag(net)
